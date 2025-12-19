eff9d0bbc878bc6c92f94c5717ca972eeebe3932
Abhishekmaheshwari123 <114015344+Abhishekmaheshwari123@users.noreply.github.com>
2025-12-19 12:21:09 +0530

    [Abhishek] Datatypes

--- core-csharp-practice/gcr-codebase/programming-elements/DataTypes.cs
+++ core-csharp-practice/gcr-codebase/programming-elements/DataTypes.cs
@@ -3,6 +3,13 @@ class DataTypes
 {
     public static void Main(String []args)
     {
-        
+        //implicitly type conversion
+        int IntVar = 100;
+        double DoubleVar = IntVar;
+        Console.WriteLine(DoubleVar);
+        //explicitly type conversion
+        double DoubleVar2 = 9.78;
+        int ExplicitIntVar = (int)DoubleVar2;
+        Console.WriteLine(ExplicitIntVar);
     }
 }
