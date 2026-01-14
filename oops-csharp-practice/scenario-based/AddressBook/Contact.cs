3007ee3779456f464854fc2bdcf292c6aaeb3905
Abhishekmaheshwari123 <114015344+Abhishekmaheshwari123@users.noreply.github.com>
2026-01-14 14:38:19 +0530

    [Abhishek] Address Bood System : UC1

---
+++ oops-csharp-practice/scenario-based/AddressBook/Contact.cs
@@ -0,0 +1,26 @@
+using System;
+class Contact
+{
+    string UserFirstName;
+    string UserLastName;
+    string UserAddress;
+    string UserCity;
+    string UserState;
+    string UserZipCode;
+    string UserPhoneNumber;
+    string UserEmail;
+
+
+    public Contact(string userFirstName, string userLastName, string userAddress, string userCity,
+            string userState, string userZipCode, string userPhoneNumber, string userEmail)
+    {        
+        UserFirstName = userFirstName;
+        UserLastName = userLastName;
+        UserAddress = userAddress;
+        UserCity = userCity;
+        UserState = userState;
+        UserZipCode = userZipCode;
+        UserPhoneNumber = userPhoneNumber;
+        UserEmail = userEmail;        
+    }
+}
